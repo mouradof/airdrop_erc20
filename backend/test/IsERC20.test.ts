5ed27abfd66ff18703a4b032a3f47f185646ff07
Alternant <mourad.hadjres@rmngp.fr>
2024-04-30 14:01:26 +0200

    finish creation smart contract test

--- backend/test/IsERC20.test.ts
+++ backend/test/IsERC20.test.ts
@@ -43,4 +43,64 @@ describe("IsERC20 Tests", function() {
       assert(contractOwner === owner.address)
     })
   })
+
+  // Mint
+  describe('Mint', function() {
+    it('should not mint if not whitelisted | @openzeppelin/merkle-tree library test',
+      async function() {
+        const { contract, merkleTree, owner, addr1, addr2 } = await loadFixture(deployContractFixture);
+        try {
+          const proof = merkleTree.getProof([addr2.address])
+          expect.fail("Expected an error 'Error: Leaf is not in tree' but none was thrown.")
+        }
+        catch(error) {
+          const err = error as Error
+          expect(err.message).to.include('Leaf is not in tree')
+        }
+      }
+    )
+  })
+
+  it('should not mint if not whitelisted | contract test', async function() {
+    const { contract, merkleTree, owner, addr1, addr2 } = await loadFixture(deployContractFixture)
+    const proof: string[] = [];
+    await expect(contract.connect(addr2).mint(addr2.address, proof)).to.be.revertedWith('Not whitelisted')
+  })
+
+  it('should not mint tokens if tokens already minted', async function() {
+    const { contract, merkleTree, owner, addr1, addr2 } = await loadFixture(deployContractFixture)
+    const proof = merkleTree.getProof([addr1.address])
+
+    await contract.connect(addr1).mint(addr1.address, proof)
+    await expect(contract.connect(addr1).mint(addr1.address, proof)).to.be.revertedWith('Tokens already minted')
+  })
+
+  it('should mint tokens if the user is whitelisted and has not minted yet', async function() {
+    const { contract, merkleTree, owner, addr1, addr2 } = await loadFixture(deployContractFixture)
+    const proof = merkleTree.getProof([addr1.address])
+
+    await contract.connect(addr1).mint(addr1.address, proof)
+
+    let balance = await contract.balanceOf(addr1.address)
+    let expectedBalance = ethers.parseEther('2')
+
+    assert(balance === expectedBalance)
+  })
+
+  // Set merkle root
+  describe('setMerkleRoot', function () {
+    it('should not set the merkle root if the caller is not the owner', async function() {
+      const { contract, merkleTree, owner, addr1, addr2 } = await loadFixture(deployContractFixture)
+      await expect(contract.connect(addr1).setMerkleRoot(merkleTree.root)).to.be.revertedWithCustomError(contract, "OwnableUnauthorizedAccount").withArgs(addr1.address)
+    })
+
+    it('should set the merkle root if the caller is the owner', async function() {
+      const { contract, merkleTree, owner, addr1, addr2 } = await loadFixture(deployContractFixture)
+      let newMerkleRoot = "0xd1000e3d5650743475aa0addfeef7e36cbfc4e060939615f4c3651e4b529d61c"
+      await contract.setMerkleRoot(newMerkleRoot)
+
+      let contractMerkleRoot = await contract.merkleRoot()
+      assert(newMerkleRoot === contractMerkleRoot)
+    })
+  })
 })
